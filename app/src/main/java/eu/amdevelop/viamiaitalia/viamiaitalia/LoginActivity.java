dd54f263daf8082d7c0937a1b6a91033e2242d05
terezamadova <tereza.madova@gmail.com>
2017-11-10 08:45:28 +0100

    nothing

---
+++ app/src/main/java/eu/amdevelop/viamiaitalia/viamiaitalia/LoginActivity.java
@@ -0,0 +1,10 @@
+package eu.amdevelop.viamiaitalia.viamiaitalia;
+
+import android.app.Activity;
+
+/**
+ * Created by terezamadova on 09/11/2017.
+ */
+
+public class LoginActivity extends Activity {
+}
